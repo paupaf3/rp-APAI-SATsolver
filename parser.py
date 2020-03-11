f6847dc9f8595c57fbb4343eef00d579802df3ba
dANI <danifarre97@gmail.com>
2020-03-11 11:48:52 +0100

    Parser ja no es un obgecte

--- parser.py
+++ parser.py
@@ -1,36 +1,33 @@
-class Parser():
 
-    def __init__(self, inputfile):
-        self.num_vars = 0
-        self.clauses = []
-        self.parse(inputfile)
+def parse(self, inputfile):
+    """ Retorna una llista de llistes, amb les clausules, si hi ha algun error,
+        retorna error. 
+    """
+    
+    num_clauses = 0
+    num_vars = 0
+    clauses = []
+    parse(inputfile)
 
-    def parse(self, inputfile):
-        """ Retorna una llista de llistes, amb les clausules, si hi ha algun error,
-            retorna error. 
-        """
-        
-        num_clauses = 0
+    with open(inputfile) as f:
+        for line in f:
+            if line[0] == "c":
+                continue
 
-        with open(inputfile) as f:
-            for line in f:
-                if line[0] == "c":
-                    continue
+            if line[0] == "p":
+                if line[1] != 'cnf':
+                    return None
 
-                if line[0] == "p":
-                    if line[1] != 'cnf':
-                        return None
-
-                    num_clauses = int(line[3])
-                    linesplit = line.split()
-                    self.num_vars = int(linesplit[2])
-                    continue
-
+                num_clauses = int(line[3])
                 linesplit = line.split()
-                linesplit.pop()
-                self.clauses.append(list(map(int, linesplit)))
+                num_vars = int(linesplit[2])
+                continue
 
-        if len(self.clauses) != num_clauses:
-            return None
+            linesplit = line.split()
+            linesplit.pop()
+            clauses.append(list(map(int, linesplit)))
 
-        return self.clauses
+    if len(clauses) != num_clauses:
+        return None
+
+    return clauses
--- solver.py
+++ solver.py
@@ -9,7 +9,7 @@ class Solver:
 
     def __init__(self, input_data):
         self.best_interpretation = interpretation.Interpretation(num_vars=input_data.num_vars)
-        self.problem = input_data.clauses
+        self.problem = input_data
         self.best_cost = self.best_interpretation.cost(self.problem)
 
     def solve(self):
@@ -31,8 +31,11 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
         sys.exit("Incorrect number of arguments, usage: ./solver.py 'input_file'\n")
 
-    parser = parser.Parser(sys.argv[1])
-    solver = Solver(parser)
+    data = parser.parse(sys.argv[1])
+    if data == None:
+        print("Input error")
+        sys.exit()
+    solver = Solver(data)
     solution = solver.solve()
     solver.show()
     #print(solution.is_solution(solver.problem))
